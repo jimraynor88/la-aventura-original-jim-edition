16a249c1d7e7267a55a49f20e44e493ef3e24fe1
Jɪᴍ Rᴀʏɴᴏʀ <232726672+jimraynor88@users.noreply.github.com>
2026-09-15 02:19:31 +0200

    Update service worker version to v3

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Cambia SW_VERSION cada vez que modifiques este archivo.
    ============================================================ */
 
-const SW_VERSION = 'v2';
+const SW_VERSION = 'v3';
 const CACHE      = 'aventura-' + SW_VERSION;
 
 const CORE = [
